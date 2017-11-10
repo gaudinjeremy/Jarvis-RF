9d3b93d93b065ef58ee4d2e37553976b7681606a
gaudinjeremy <gaudin.jeremy@gmail.com>
2017-11-10 23:01:52 +0100

    1.1
    Ajout des fonctions

--- functions.sh
+++ functions.sh
@@ -2,36 +2,12 @@
 
 #MODULE RF pour RadioFrequence
 
-js_rf_switch(){
+js_rf_on(){
 
-    if [ $3 = "on" ]
-    then
-        old="off"
-    else
-        old="on"
-    fi
-
-    deviceId=$(js_ia_getStrictText $1 $2)
-    sudo plugins/jarvis-rf/chacon 0 $deviceId 1 $3
-    js_ia_upCell $old $3 $1
+    sudo plugins_installed/jarvis-rf/chacon 0 00000001 1 on
 }
 
-js_rf_switchGroup() {
+js_rf_off(){
 
-    group=$1
-
-    row=$(js_ia_loopCell $group)
-    if [ $row != 'false' ]
-    then
-        nb=$(js_ia_countCells $row)
-        for ((i=1; i<=$nb; i++))
-        do
-            name=$(js_ia_getCell $row $i)
-            js_rf_switch $name 3 $2
-        done
-        js_ia_say "confirmation"
-    else
-        js_ia_say "erreur"
-        say "je n'ai pas trouvé le groupe"
-    fi
+    sudo plugins_installed/jarvis-rf/chacon 0 00000001 1 on
 }

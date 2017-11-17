0022247936947a91e1ecb15ae35984c33c6a6c1d
Jérémy GAUDIN <gaudin.jeremy@gmail.com>
2017-11-17 14:54:03 +0100

    TEST 1.0

--- functions.sh
+++ functions.sh
@@ -11,3 +11,11 @@ js_rf_off(){
 
     sudo plugins_installed/Jarvis-RF/chacon 0 00000001 1 off
 }
+
+# $1 : clef de la prise
+# $2 : on / off
+jv_rf_switch() {
+
+    prise=$(jv_ia getValue RF $1)
+    sudo $rf_path 0 prise $2
+}
